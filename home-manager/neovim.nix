69536dc3e164fbc5118a293bde215e96e7ae13bb
dxcently <dxcently@gmail.com>
2025-02-23 14:02:38 -0500

    changed nvim colors to windows nt

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -24,7 +24,24 @@ in {
         enable = true;
         name = "base16";
         base16-colors = {
-          base00 = "${palette.base00}";
+          base00 = "000000";
+          base01 = "2a2a2a";
+          base02 = "555555";
+          base03 = "808080";
+          base04 = "a1a1a1";
+          base05 = "c0c0c0";
+          base06 = "e0e0e0";
+          base07 = "ffffff";
+          base08 = "ff0000";
+          base09 = "808000";
+          base0A = "ffff00";
+          base0B = "00ff00";
+          base0C = "00ffff";
+          base0D = "0000ff";
+          base0E = "ff00ff";
+          base0F = "008000";
+          /*
+           base00 = "${palette.base00}";
           base01 = "${palette.base01}";
           base02 = "${palette.base02}";
           base03 = "${palette.base03}";
@@ -39,7 +56,8 @@ in {
           base0C = "${palette.base0C}";
           base0D = "${palette.base0D}";
           base0E = "${palette.base0E}";
-          base0F = "${palette.base0F}";
+          base0F = "4D4D4D"; #0F too dark
+          */
         };
       };
 
