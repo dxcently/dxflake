9537370a4faf9fc9959d8945e5ef7b9a9268820f
dxcently <dxcently@gmail.com>
2025-03-15 14:07:42 -0400

    moved services configs

--- hosts/dxeon/default.nix
+++ hosts/dxeon/default.nix
@@ -89,38 +89,6 @@
 
   #services
   services = {
-    openssh = {
-      enable = true;
-      settings = {
-        # Forbid root login through SSH.
-        PermitRootLogin = "no";
-        # Use keys only. Remove if you want to SSH using password (not recommended)
-        PasswordAuthentication = true;
-      };
-    };
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-    blueman.enable = true;
-    devmon.enable = true;
-    gvfs.enable = true;
-    udisks2.enable = true;
-    flatpak.enable = true;
-    tumbler.enable = true;
-    printing.enable = true;
-    avahi = {
-      enable = true;
-      nssmdns = true;
-      openFirewall = true;
-    };
-    jellyfin = {
-      enable = true;
-      package = pkgs.jellyfin;
-    };
     syncthing = {
       enable = true;
       user = "khoa";
