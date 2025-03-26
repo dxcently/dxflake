c1c4533a69f2dad2612f748fa34e3e3b165b9b89
dxcently <dxcently@gmail.com>
2025-03-26 13:25:56 -0400

    using hyprpaper

--- modules/home/hyprpaper.nix
+++ modules/home/hyprpaper.nix
@@ -5,5 +5,11 @@
 }: {
   services.hyprpaper = {
     enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
+      preload = ["~/dxflake/extras/wallpapers/ssr_tile.png"];
+      wallpaper = [", tile:~/dxflake/extras/wallpapers/ssr_tile.png"];
+    };
   };
 }
