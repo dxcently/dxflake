b3ea1064454c1a113dcb0aa4185818d8169fdeed
dxcently <dxcently@gmail.com>
2025-04-05 18:16:59 -0400

    removed lsplines

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -151,7 +151,6 @@ in {
         trouble.enable = true;
         lspSignature.enable = true;
         otter-nvim.enable = false;
-        lsplines.enable = false;
         nvim-docs-view.enable = false;
       };
 
