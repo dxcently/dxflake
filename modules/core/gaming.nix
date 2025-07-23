0d6607bee290368b32a9a42cc91cdef148589493
dxcently <dxcently@gmail.com>
2025-07-23 02:00:48 -0400

    added gaming.nix

---
+++ modules/core/gaming.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [inputs.aagl.nixosModules.default];
+  nix.settings = inputs.aagl.nixConfig;
+  programs = {
+    honkers-railway-launcher.enable = true;
+    anime-game-launcher.enable = true;
+  };
+}
