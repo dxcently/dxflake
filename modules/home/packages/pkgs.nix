e6c60c1f20255afa1c55a14727db6f5d0716feb0
dxcently <dxcently@gmail.com>
2025-09-08 11:29:40 -0400

    pkgs.nix moved to home

---
+++ modules/home/packages/pkgs.nix
@@ -0,0 +1,124 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  #universal packages
+  environment.systemPackages = with pkgs; [
+    # de stuff
+    swww
+    waybar
+    networkmanagerapplet
+    kitty
+    dunst
+    slurp
+    grim
+    swappy
+    bash
+    wpgtk
+    wl-clipboard
+    polkit_gnome
+    libnotify
+    unrar
+    unzip
+    yad
+    lm_sensors
+    cliphist
+    brightnessctl
+    mpv
+    gparted
+    kdePackages.gwenview
+    feh
+    hyprpaper
+    # audio
+    wireplumber
+    pavucontrol
+    # programs
+    jellyfin-media-player
+    ngrok
+    rpcs3
+    scrcpy
+    starship
+    osu-lazer-bin
+    librewolf
+    arduino-ide
+    chromium
+    tor-browser
+    firefox
+    anki-bin
+    vscodium
+    neovide
+    nicotine-plus
+    webcamoid
+    lutris
+    wine
+    protonup-qt
+    vlc
+    strawberry
+    soundconverter
+    (vesktop.override {electron = pkgs.electron_35;})
+    bottles
+    qbittorrent
+    obs-studio
+    #jellyfin
+    #jellyfin-web
+    #jellyfin-ffmpeg
+    #jellyfin-media-player
+    #parsec-bin
+    zoom-us
+    youtube-music
+    calibre
+    jupyter
+    gimp3-with-plugins
+    inkscape
+    # cli programs
+    vim
+    socat
+    bat
+    gh # github gui
+    git
+    lazygit
+    xdg-utils
+    lshw
+    lsd
+    fastfetch
+    cowsay
+    fzf
+    ripgrep
+    atuin
+    zoxide
+    btop
+    nh
+    wget
+    clinfo
+    ydotool
+    curl
+    ffmpegthumbnailer
+    ffmpeg
+    unar
+    jq
+    poppler
+    fd
+    file
+    ripgrep
+    spotdl
+    yt-dlp
+    playerctl
+    gcalcli
+    # libs/frameworks
+    qt6.qtwayland
+    libsForQt5.qt5.qtwayland
+    ninja
+    python3
+    meson
+    nodejs
+    pkg-config
+    v4l-utils
+    nixfmt-rfc-style
+    typst
+    tinymist
+    gcc
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = ["electron-32.3.3"];
+}
