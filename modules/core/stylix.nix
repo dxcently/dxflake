0108066670ee7f2b93f2f9bfdb35dd2f7538fa6e
dxcently <dxcently@gmail.com>
2025-09-13 00:03:03 -0400

    changed colors

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -8,24 +8,24 @@
     polarity = "light";
     opacity.terminal = 1.0;
     base16Scheme = {
-      #name: Atelier Seaside Light
-      #author: Bram de Haan (http://atelierbramdehaan.nl)
-      base00 = "f4fbf4";
-      base01 = "cfe8cf";
-      base02 = "8ca68c";
-      base03 = "809980";
-      base04 = "687d68";
-      base05 = "5e6e5e";
-      base06 = "242924";
-      base07 = "131513";
-      base08 = "e6193c";
-      base09 = "87711d";
-      base0A = "98981b";
-      base0B = "29a329";
-      base0C = "1999b3";
-      base0D = "3d62f5";
-      base0E = "ad2bee";
-      base0F = "e619c3";
+      #scheme: "Equilibrium Light"
+      #author: "Carlo Abelli"
+      base00 = "f5f0e7";
+      base01 = "e7e2d9";
+      base02 = "d8d4cb";
+      base03 = "73777f";
+      base04 = "5a5f66";
+      base05 = "43474e";
+      base06 = "2c3138";
+      base07 = "181c22";
+      base08 = "d02023";
+      base09 = "bf3e05";
+      base0A = "9d6f00";
+      base0B = "637200";
+      base0C = "007a72";
+      base0D = "0073b5";
+      base0E = "4e66b6";
+      base0F = "c42775";
     };
     cursor = {
       size = 28;
