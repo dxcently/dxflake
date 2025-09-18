130729a1df8d7249c7611e53f870b314a28e8527
dxcently <dxcently@gmail.com>
2025-09-18 17:21:29 -0400

    hyprland keybinds

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -77,14 +77,16 @@
       bind = SUPER ALT, J, resizeactive, 0 20
       bind = SUPER ALT, K, resizeactive, 0 -20
       bind = SUPER ALT, L, resizeactive, 20 0
-      bind = ALT, Tab, cyclenext
-      bind = ALT, Tab, bringactivetotop
+      #bind = SUPER SHIFT, bringactivetotop
+      #bind = ALT, Tab, cyclenext
+      bind = ALT, Tab, workspace, previous
 
       bind = SUPER, 1, workspace, 1
       bind = SUPER, 2, workspace, 2
       bind = SUPER, 3, workspace, 3
       bind = SUPER, 4, workspace, 4
       bind = SUPER, 5, workspace, 5
+      bind = SUPER, D, workspace, 5
       bind = SUPER, 6, workspace, 6
       bind = SUPER, 7, workspace, 7
       bind = SUPER, 8, workspace, 8
