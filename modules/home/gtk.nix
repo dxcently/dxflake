34f0bac6dfc35095ccabe3107f5e8c693b7a203e
dxcently <dxcently@gmail.com>
2025-09-20 02:58:12 -0400

    prefer dark

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -10,10 +10,10 @@
       package = pkgs.faba-icon-theme;
     };
     gtk3.extraConfig = {
-      gtk-application-prefer-light-theme = 1;
+      gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
-      gtk-application-prefer-light-theme = 1;
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
