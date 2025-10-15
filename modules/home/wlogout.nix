9dcdc03d17eff4dca8e28bf3d6c8005253548eb3
dxcently <dxcently@gmail.com>
2025-10-14 20:31:49 -0400

    configured wlogout

--- modules/home/wlogout.nix
+++ modules/home/wlogout.nix
@@ -6,5 +6,43 @@
 }: {
   programs.wlogout = {
     enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "hyprlock";
+        text = "Lock";
+        keybind = "1";
+      }
+      {
+        label = "logout";
+        action = "hyprctl dispatch exit";
+        text = "Logout";
+        keybind = "2";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Sleep";
+        keybind = "3";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "4";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "5";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "6";
+      }
+    ];
   };
 }
