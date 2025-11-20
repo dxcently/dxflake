7969f0e366142ae77c1b36a3a1d13df1b9d24895
dxcently <dxcently@gmail.com>
2025-11-20 02:31:41 -0500

    TODO

--- modules/home/obsidian.nix
+++ modules/home/obsidian.nix
@@ -5,7 +5,8 @@
 }: {
   programs.obsidian = {
     enable = true;
-    vaults.obsidian-vaults = {
+    /*
+      vaults.obsidian-vaults = {
       enable = true;
       target = "Documents/Magi";
     };
@@ -14,27 +15,6 @@
         vimMode = true;
       };
     };
-    corePlugins = [
-      "file-explorer"
-      "command-palette"
-      "graph-view"
-      "daily-notes"
-      "canvas"
-      "bases"
-      "backlinks"
-      "bookmarks"
-      "file-recovery"
-      "files"
-      "note-composer"
-      "outgoing-links"
-      "outline"
-      "page-preview"
-      "quick-switcher"
-      "search"
-      "slash-commands"
-      "slides"
-      "tags-view"
-      "word-count"
-    ];
+    */
   };
 }
