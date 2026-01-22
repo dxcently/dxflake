2ae656d046abbfbc13f20c83ebbae59cf4d429b2
dxcently <dxcently@gmail.com>
2026-01-22 03:48:42 -0500

    solaar

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:ezKEa/aagl-gtk-on-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    solaar = {
+      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = {
     nixpkgs,
--- modules/core/solaar.nix
+++ modules/core/solaar.nix
@@ -1,8 +1,17 @@
 {
   pkgs,
-  config,
-  solaar,
+  inputs,
   ...
 }: {
-  services.solaar.enable = true;
+  imports = [
+    inputs.solaar.nixosModules.default
+  ];
+
+  services.solaar = {
+    enable = true; # Enable the service
+    package = pkgs.solaar; # The package to use
+    window = "hide"; # Show the window on startup (show, *hide*, only [window only])
+    batteryIcons = "regular"; # Which battery icons to use (*regular*, symbolic, solaar)
+    extraArgs = ""; # Extra arguments to pass to solaar on startup
+  };
 }
