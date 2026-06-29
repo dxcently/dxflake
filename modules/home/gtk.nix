be3ccb8add10055fc944eb54d373ab5c7e4404ef
dxcently <dxcently@gmail.com>
2026-06-28 22:45:42 -0400

    removed null gtk4 theme

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -8,11 +8,8 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
-    gtk4 = {
-      theme = null;
-      extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
