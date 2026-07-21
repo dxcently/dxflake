6128338bc87aa94ec8bf2307ca37882c87352031
dxcently <dxcently@gmail.com>
2026-07-21 03:25:49 -0400

    hyprland: open obsidian on magic workspace, scroll layout

--- modules/dendrites/hyprland/default.nix
+++ modules/dendrites/hyprland/default.nix
@@ -120,12 +120,19 @@
             match:title = ^(YT Music)$
           }
 
+
+          windowrule {
+            name = windowrule-6
+            workspace = special:magic
+            match:class = ^([Oo]bsidian)$
+          }
+
           ";
             # Workspace Assignments
             workspace = [
               "n, monitor:HDMI-A-1, default:true"
               "special:discord, monitor:HDMI-A-1, on-created-empty:vesktop"
-              "special:magic, on-created-empty:pear-desktop"
+              "special:magic, on-created-empty:pear-desktop, layout:scrolling"
               "2, monitor:HDMI-A-1, persistent:true"
               "special:replay, on-created-empty:gpu-screen-recorder-gtk"
               # Every workspace on the vertical monitor uses the scrolling layout.
